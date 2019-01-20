822ad570089535d598aae00bdea9f191ad022496
sithu <sithuaung223@gmail.com>
2019-01-20 14:35:16 -0600

    reviewed from Alex

--- test_playing.py
+++ test_playing.py
@@ -8,42 +8,34 @@ test_hand = [
     Card(Rank.ace, Suit.spade),
     Card(Rank.king, Suit.diamond)]
 
-
 def test_play_acard_twoheart_losetwoheartfromhand():
     result_hand = play_acard(test_hand, Card(Rank.two, Suit.heart))
-
     assert len(result_hand) == 3
     assert Card(Rank.two, Suit.heart) not in result_hand
 
-
-def test_decide_winner_samesuits_fiveofheartwins():
+def test_decide_winner_samesuits_highestrankwins():
     playing_cards = [
         Card(Rank.two, Suit.heart),
         Card(Rank.three, Suit.heart),
         Card(Rank.four, Suit.heart),
         Card(Rank.five, Suit.heart)]
     winning_card = decide_winner(playing_cards)
-
     assert winning_card == Card(Rank.five, Suit.heart)
 
-
-def test_decide_winner_offsuitswithoutspade_fourofheartwins():
+def test_decide_winner_offsuitswithoutspade_highestrankofleadingsuitwins():
     playing_cards = [
         Card(Rank.two, Suit.heart),
         Card(Rank.three, Suit.diamond),
         Card(Rank.four, Suit.heart),
         Card(Rank.five, Suit.club)]
     winning_card = decide_winner(playing_cards)
-
     assert winning_card == Card(Rank.four, Suit.heart)
 
-
-def test_decide_winner_offsuitswithaspade_threeofspadewins():
+def test_decide_winner_offsuitswithaspade_highestspadewins():
     playing_cards = [
         Card(Rank.two, Suit.heart),
         Card(Rank.three, Suit.spade),
-        Card(Rank.four, Suit.heart),
+        Card(Rank.four, Suit.spade),
         Card(Rank.five, Suit.club)]
     winning_card = decide_winner(playing_cards)
-
-    assert winning_card == Card(Rank.three, Suit.spade)
+    assert winning_card == Card(Rank.four, Suit.spade)
